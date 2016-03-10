e2199f89968aba442d072a7c9ea7615f430fd9a7
james murphy <djlotus@jamess-MacBook-Pro.local>
2016-03-10 14:48:22 -0500

    updated nav to use [data-nav] as opposed to classes

--- js/_navigation.js
+++ js/_navigation.js
@@ -20,72 +20,53 @@
 
 "use-strict";                 // Lock script in strict mode. May cause issues later.
 
-var dropClass =               "dropdown", // Set class for dropClass
-    elementArray =            document.getElementsByClassName(dropClass), // create HTML collection of all elements with class dropdown
-    topLevel =                Array.prototype.slice.call(elementArray); // convert HTML collection to Array
+var topLevel =                document.querySelectorAll("[data-nav]"), //Grab nav elements which contain sub-navs
+    navElement =              Array.prototype.slice.call(topLevel); //Convert HTML collection to actual Array
 
-// Add focus event to anchor tags to allow keyboard accessibility.
-// - - - - - - - - - - - - - - - - - - - -
-
-for (var i = 0; i <= topLevel.length - 1; i++) {
-   // Use AddEventListener for happy browsers
+for(var i = 0; i <= topLevel.length - 1; i++) {
+   // Use addEventListener for happy browsers
    if (document.addEventListener) {
-      topLevel[i].addEventListener("focus", function(e) {
-         if (e.target && e.target.className == "sub_link") {
-            console.log("You focused on a sublink");
-            this.className = "dropdown active";
-         } else {
-            console.log("It's not working. Need more coffee apparently...");
-         }
-      }, true);
+      navElement[i].addEventListener("focus", function(e) {
+      if (e.target && e.target.className == "sub_link") {
+         this.setAttribute("data-nav", "true");
+         console.log("You focused on a dropdown element");
+      } else {
+         console.log("You focused on a top level element");
+      }
+   }, true);
       // Remove focus event: prevents more than one menu from displaying at a time
-      topLevel[i].addEventListener("blur", function(e) {
+      navElement[i].addEventListener("blur", function(e) {
          if (e.target && e.target.className == "sub_link") {
-            console.log("2");
-            this.className = "dropdown";
+            this.setAttribute("data-nav", null);
          }
       }, true);
    // Use attachEvent for sad browsers :(
    } else if (document.attachEvent) {
-      topLevel[i].attachEvent("focus", function(e) {
+      navElement[i].attachEvent("focus", function(e) {
+         if (e.target && e.target.className == "sub_link") {
+            this.setAttribute("data-nav", "true");
+            console.log("You focused on a dropdown element");
+         } else {
+            console.log("You focused on a top level element");
+         }
+      }, true);
+      navElement[i].attachEvent("focus", function(e) {
          var target = e.target || e.srcElement;
          if (target && target.className == "sub_link") {
             console.log("You focused on a sublink");
-            this.className = "dropdown active";
+            this.setAttribute("data-nav", null);
          } else {
             console.log("There is something wrong with your script in IE8 . It's not working dummy");
          }
       }, true);
       // Remove focus event: prevents more than one menu from displaying at a time
-      topLevel[i].attachEvent("blur", function(e) {
+      navElement[i].attachEvent("blur", function(e) {
          var target = e.target || e.srcElement;
          if (target && target.className == "sub_link") {
             console.log("2");
-            this.className = "dropdown";
+            this.setAttribute("data-nav", null);
          }
       }, true);
    }
-};
-
-// Add click event for ShowNav on mobile
-// - - - - - - - - - - - - - - - - - - - -
-
-var navButton =               document.getElementById("showNav"), // Set nav button
-    mainNav =                 document.getElementById("main-nav"), // Set main navigation element
-    isClicked =               "false"; // Set clicked status to false by default
-
-if (document.addEventListener) {
-   navButton.addEventListener("click", function(e) {
-      if (isClicked === "false") { // if nav not active, show nav
-         console.log("main nav opened");
-         navButton.innerHTML = "Hide Menu";
-         mainNav.className = "active";
-         isClicked = "true";
-      } else if (isClicked === "true") { // if nav is active, hide nav
-         console.log("main nav closed");
-         navButton.innerHTML = "Show Menu";
-         mainNav.className = "";
-         isClicked = "false";
-      }
-   });
 }
+   
