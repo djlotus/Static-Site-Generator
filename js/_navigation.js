2f0e86816d2cae1aac6506ea11db8076406a12c3
james murphy <djlotus@jamess-MacBook-Pro.local>
2016-03-10 15:32:20 -0500

    updated mobile nav button to use [data-nav] and dropdown navigaton items to use [data-dropdown]

--- js/_navigation.js
+++ js/_navigation.js
@@ -20,15 +20,18 @@
 
 "use-strict";                 // Lock script in strict mode. May cause issues later.
 
-var topLevel =                document.querySelectorAll("[data-nav]"), //Grab nav elements which contain sub-navs
-    navElement =              Array.prototype.slice.call(topLevel); //Convert HTML collection to actual Array
+// Add Event Listeners to navigation to allow for keyboard navigation
+// - - - - - - - - - - - - - - - - - - - -
+
+var topLevel =                document.querySelectorAll("[data-dropdown]"), // Grab nav elements which contain sub-navs
+    navElement =              Array.prototype.slice.call(topLevel); // Convert HTML collection to actual Array
 
 for(var i = 0; i <= topLevel.length - 1; i++) {
    // Use addEventListener for happy browsers
    if (document.addEventListener) {
       navElement[i].addEventListener("focus", function(e) {
       if (e.target && e.target.className == "sub_link") {
-         this.setAttribute("data-nav", "true");
+         this.setAttribute("data-dropdown", "true");
          console.log("You focused on a dropdown element");
       } else {
          console.log("You focused on a top level element");
@@ -37,14 +40,14 @@ for(var i = 0; i <= topLevel.length - 1; i++) {
       // Remove focus event: prevents more than one menu from displaying at a time
       navElement[i].addEventListener("blur", function(e) {
          if (e.target && e.target.className == "sub_link") {
-            this.setAttribute("data-nav", null);
+            this.setAttribute("data-dropdown", null);
          }
       }, true);
    // Use attachEvent for sad browsers :(
    } else if (document.attachEvent) {
       navElement[i].attachEvent("focus", function(e) {
          if (e.target && e.target.className == "sub_link") {
-            this.setAttribute("data-nav", "true");
+            this.setAttribute("data-dropdown", "true");
             console.log("You focused on a dropdown element");
          } else {
             console.log("You focused on a top level element");
@@ -54,7 +57,7 @@ for(var i = 0; i <= topLevel.length - 1; i++) {
          var target = e.target || e.srcElement;
          if (target && target.className == "sub_link") {
             console.log("You focused on a sublink");
-            this.setAttribute("data-nav", null);
+            this.setAttribute("data-dropdown", null);
          } else {
             console.log("There is something wrong with your script in IE8 . It's not working dummy");
          }
@@ -64,9 +67,31 @@ for(var i = 0; i <= topLevel.length - 1; i++) {
          var target = e.target || e.srcElement;
          if (target && target.className == "sub_link") {
             console.log("2");
-            this.setAttribute("data-nav", null);
+            this.setAttribute("data-dropdown", null);
          }
       }, true);
    }
 }
-   
+
+// Add Event Listener to hide/show navigation on mobile
+// - - - - - - - - - - - - - - - - - - - -
+
+var navButton =               document.getElementById("showNav"), // Set nav button
+    mainNav =                 document.querySelector("[data-nav]"), // Set main nav element
+    isClicked =               "false"; // Set clicked status to false by default
+
+if (document.addEventListener) {
+   navButton.addEventListener("click", function(e) {
+      if (isClicked === "false") { // if nav not active, show nav
+         console.log("mobile nav is open"); // log the state of the navigation element
+         navButton.innerHTML = "Hide Menu"; // change text in button to reflect the action of the button
+         mainNav.setAttribute("data-nav", "true"); // show the navigation element
+         isClicked = "true"; // update value of isClicked
+      } else if (isClicked === "true") { // if nav is active, hide nav
+         console.log("mobile nav is closed"); // log the state of the navigation element
+         navButton.innerHTML = "Show Menu"; // change text in button to reflect the action of the button
+         mainNav.setAttribute("data-nav", "false"); // hide the navigation element
+         isClicked = "false"; // update value of isClicked
+      }
+   })
+}
