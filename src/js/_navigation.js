3ed0b8d266edde24b7196eb95221911489afbd11
djlotus <lotusdj1@gmail.com>
2021-02-02 15:50:53 -0500

    Updated sticky nav, Updated header to grid, Added new header options, Added FontAwesome icons, Removed IcoMoon icons, Updated button styles, Updated HTML template pages with better comments for nav options, Added placeholder images from Placeholder.com, Updated "sass" script flags, Updated "build" script with copyFiles command.

--- src/js/_navigation.js
+++ src/js/_navigation.js
@@ -1,36 +1,18 @@
-
-
 // - - - - - - - - - - - - - - - - - - - -
 // Add Event Listeners to navigation to allow for keyboard navigation
 // - - - - - - - - - - - - - - - - - - - -
 
-var topLevel =                document.querySelectorAll("[data-dropdown]"), // Grab nav elements which contain sub-navs
-    navElement =              []; // Create empty array for navigation elements
+var topLevel = document.querySelectorAll("[data-dropdown]"), // Grab nav elements which contain sub-navs
+   navElement = []; // Create empty array for navigation elements
 
 for (var i = 0; i < topLevel.length; i++) {
    navElement.push(topLevel[i]);
 }
 
-for(var i = 0; i <= topLevel.length - 1; i++) {
+for (var i = 0; i <= topLevel.length - 1; i++) {
    // Use addEventListener for happy browsers
    if (document.addEventListener) {
-      navElement[i].addEventListener("focus", function(e) {
-      if (e.target && e.target.className == "sub_link") {
-         this.setAttribute("data-dropdown", "true");
-         console.log("You focused on a dropdown element");
-      } else {
-         console.log("You focused on a top level element");
-      }
-   }, true);
-      // Remove focus event: prevents more than one menu from displaying at a time
-      navElement[i].addEventListener("blur", function(e) {
-         if (e.target && e.target.className == "sub_link") {
-            this.setAttribute("data-dropdown", null);
-         }
-      }, true);
-   // Use attachEvent for sad browsers :(
-   } else if (document.attachEvent) {
-      navElement[i].attachEvent("onfocus", function(e) {
+      navElement[i].addEventListener("focus", function (e) {
          if (e.target && e.target.className == "sub_link") {
             this.setAttribute("data-dropdown", "true");
             console.log("You focused on a dropdown element");
@@ -38,7 +20,23 @@ for(var i = 0; i <= topLevel.length - 1; i++) {
             console.log("You focused on a top level element");
          }
       }, true);
-      navElement[i].attachEvent("onfocus", function(e) {
+      // Remove focus event: prevents more than one menu from displaying at a time
+      navElement[i].addEventListener("blur", function (e) {
+         if (e.target && e.target.className == "sub_link") {
+            this.setAttribute("data-dropdown", null);
+         }
+      }, true);
+      // Use attachEvent for sad browsers :(
+   } else if (document.attachEvent) {
+      navElement[i].attachEvent("onfocus", function (e) {
+         if (e.target && e.target.className == "sub_link") {
+            this.setAttribute("data-dropdown", "true");
+            console.log("You focused on a dropdown element");
+         } else {
+            console.log("You focused on a top level element");
+         }
+      }, true);
+      navElement[i].attachEvent("onfocus", function (e) {
          var target = e.target || e.srcElement;
          if (target && target.className == "sub_link") {
             console.log("You focused on a sublink");
@@ -48,7 +46,7 @@ for(var i = 0; i <= topLevel.length - 1; i++) {
          }
       }, true);
       // Remove focus event: prevents more than one menu from displaying at a time
-      navElement[i].attachEvent("onblur", function(e) {
+      navElement[i].attachEvent("onblur", function (e) {
          var target = e.target || e.srcElement;
          if (target && target.className == "sub_link") {
             console.log("2");
@@ -63,13 +61,13 @@ for(var i = 0; i <= topLevel.length - 1; i++) {
 // Add Event Listener to hide/show navigation on mobile
 // - - - - - - - - - - - - - - - - - - - -
 
-var navButton =               document.getElementById("showNav"), // Set nav button
-    mainNav =                 document.querySelector("[data-nav]"), // Set main nav element
-    navContainer =            mainNav.parentElement, // Set parent container
-    isClicked =               "false"; // Set clicked status to false by default
+var navButton = document.getElementById("showNav"), // Set nav button
+   mainNav = document.querySelector("[data-nav]"), // Set main nav element
+   navContainer = mainNav.parentElement, // Set parent container
+   isClicked = "false"; // Set clicked status to false by default
 
 if (document.addEventListener) {
-   navButton.addEventListener("click", function(e) {
+   navButton.addEventListener("click", function (e) {
       if (isClicked === "false") { // if nav not active, show nav
          console.log("mobile nav is open"); // log the state of the navigation element
          navButton.innerHTML = "<br /><span>Hide</span>"; // change text in button to reflect the action of the button
@@ -93,9 +91,9 @@ if (document.addEventListener) {
 // Set top offset for header element on mobile
 // - - - - - - - - - - - - - - - - - - - -
 
-var docBody =                 document.body, // Get body
-    navbarElement =           document.getElementById("show-nav"), // Get mobile navbar element
-    navbarOffset =            navbarElement.scrollHeight; // Get height of mobile navbar element
+var docBody = document.body, // Get body
+   navbarElement = document.getElementById("show-nav"), // Get mobile navbar element
+   navbarOffset = navbarElement.offsetHeight; // Get height of mobile navbar element
 
 docBody.style.marginTop = navbarOffset + "px";
 
@@ -104,16 +102,17 @@ docBody.style.marginTop = navbarOffset + "px";
 // Position Sticky Navbar
 // - - - - - - - - - - - - - - - - - - - -
 
-var header =                  document.querySelector("[role=banner]"), // Get header element
-    headerOffset =            header.scrollHeight, // Get height of header element
-    stickyNav =               document.querySelector("[data-sticky]"), // Get sticky nav element if exists
-    regNav =                  document.querySelector("[role=navigation]"); // get first instance of navigation element
+var header = document.querySelector("[role=banner]"), // Get header element
+   headerOffset = header.offsetHeight, // Get height of header element
+   stickyNav = document.querySelector("[data-sticky]"), // Get sticky nav element if exists
+   regNav = document.querySelector("[role=navigation]"), // get first instance of navigation element
+   stickyOffset = stickyNav.offsetHeight; // set height of navigation bar
 
 if (regNav.hasAttribute("data-nav-align-top")) {
    regNav.className = "sticky";
 } else {
    if (document.addEventListener) { // If happy browser
-      window.addEventListener("scroll", function() { // add scroll event
+      window.addEventListener("scroll", function () { // add scroll event
          if (stickyNav === regNav) { // if sticky nav element is the same as first instance of navigation
             if (window.pageYOffset >= headerOffset) { // if window is scrolled equal to or more than the height of the header element
                regNav.className = "sticky"; // add class "sticky" to nav element
@@ -123,7 +122,7 @@ if (regNav.hasAttribute("data-nav-align-top")) {
          }
       })
    } else if (document.attachEvent) { // If sad browser :( nav should simply not stick
-      window.attachEvent("onscroll", function() { // add scroll event
+      window.attachEvent("onscroll", function () { // add scroll event
          if (stickyNav === regNav) { // if sticky nav element is the same as first instance of navigation
             if (window.pageYOffset >= headerOffset) { // if window is scrolled equal to or more than the height of the header element
                regNav.className = "sticky"; // add class "sticky" to nav element
