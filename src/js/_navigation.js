5ff3f7c6f61703ba14ca587206fe8ef13107a830
djlotus <lotusdj1@gmail.com>
2023-04-06 09:28:06 -0400

    updated scss to use calc for division

--- src/js/_navigation.js
+++ src/js/_navigation.js
@@ -12,50 +12,71 @@ for (var i = 0; i < topLevel.length; i++) {
 for (var i = 0; i <= topLevel.length - 1; i++) {
    // Use addEventListener for happy browsers
    if (document.addEventListener) {
-      navElement[i].addEventListener("focus", function (e) {
-         if (e.target && e.target.className == "sub_link") {
-            this.setAttribute("data-dropdown", "true");
-            console.log("You focused on a dropdown element");
-         } else {
-            console.log("You focused on a top level element");
-         }
-      }, true);
+      navElement[i].addEventListener(
+         "focus",
+         function (e) {
+            if (e.target && e.target.className == "sub_link") {
+               this.setAttribute("data-dropdown", "true");
+               console.log("You focused on a dropdown element");
+            } else {
+               console.log("You focused on a top level element");
+            }
+         },
+         true
+      );
       // Remove focus event: prevents more than one menu from displaying at a time
-      navElement[i].addEventListener("blur", function (e) {
-         if (e.target && e.target.className == "sub_link") {
-            this.setAttribute("data-dropdown", null);
-         }
-      }, true);
+      navElement[i].addEventListener(
+         "blur",
+         function (e) {
+            if (e.target && e.target.className == "sub_link") {
+               this.setAttribute("data-dropdown", null);
+            }
+         },
+         true
+      );
       // Use attachEvent for sad browsers :(
    } else if (document.attachEvent) {
-      navElement[i].attachEvent("onfocus", function (e) {
-         if (e.target && e.target.className == "sub_link") {
-            this.setAttribute("data-dropdown", "true");
-            console.log("You focused on a dropdown element");
-         } else {
-            console.log("You focused on a top level element");
-         }
-      }, true);
-      navElement[i].attachEvent("onfocus", function (e) {
-         var target = e.target || e.srcElement;
-         if (target && target.className == "sub_link") {
-            console.log("You focused on a sublink");
-            this.setAttribute("data-dropdown", null);
-         } else {
-            console.log("There is something wrong with your script in IE8 . It's not working dummy");
-         }
-      }, true);
+      navElement[i].attachEvent(
+         "onfocus",
+         function (e) {
+            if (e.target && e.target.className == "sub_link") {
+               this.setAttribute("data-dropdown", "true");
+               console.log("You focused on a dropdown element");
+            } else {
+               console.log("You focused on a top level element");
+            }
+         },
+         true
+      );
+      navElement[i].attachEvent(
+         "onfocus",
+         function (e) {
+            var target = e.target || e.srcElement;
+            if (target && target.className == "sub_link") {
+               console.log("You focused on a sublink");
+               this.setAttribute("data-dropdown", null);
+            } else {
+               console.log(
+                  "There is something wrong with your script in IE8 . It's not working dummy"
+               );
+            }
+         },
+         true
+      );
       // Remove focus event: prevents more than one menu from displaying at a time
-      navElement[i].attachEvent("onblur", function (e) {
-         var target = e.target || e.srcElement;
-         if (target && target.className == "sub_link") {
-            console.log("2");
-            this.setAttribute("data-dropdown", null);
-         }
-      }, true);
+      navElement[i].attachEvent(
+         "onblur",
+         function (e) {
+            var target = e.target || e.srcElement;
+            if (target && target.className == "sub_link") {
+               console.log("2");
+               this.setAttribute("data-dropdown", null);
+            }
+         },
+         true
+      );
    }
 }
-
 
 // - - - - - - - - - - - - - - - - - - - -
 // Add Event Listener to hide/show navigation on mobile
@@ -69,35 +90,37 @@ var isClicked = "false"; // Set clicked status to false by default
 
 if (document.addEventListener) {
    navButton.addEventListener("click", function (e) {
-      if (isClicked === "false") { // if nav not active, show nav
+      navButton.classList.toggle("fa-bars");
+      navButton.classList.toggle("fa-xmark");
+      if (isClicked === "false") {
+         // if nav not active, show nav
          //console.log("mobile nav is open"); // log the state of the navigation element
-         navButton.innerHTML = "<br /><span>Hide Navigation</span>"; // change text in button to reflect the action of the button
+         navButton.innerHTML = "<span> &nbsp;Hide Navigation</span>"; // change text in button to reflect the action of the button
          mainNav.setAttribute("data-nav", "true"); // show the navigation element
          isClicked = "true"; // update value of isClicked
          navContainer.style.width = "100%"; // allow container to show
-         navButton.classList.add("icon-menu"); // show close icon
-      } else if (isClicked === "true") { // if nav is active, hide nav
+         // navButton.classList.add("fa-bars"); // show menu icon
+         // navButton.classList.remove("fa"); // hide close icon
+      } else if (isClicked === "true") {
+         // if nav is active, hide nav
          //console.log("mobile nav is closed"); // log the state of the navigation element
-         navButton.innerHTML = "<br /><span>Show Navigation</span>"; // change text in button to reflect the action of the button
+         navButton.innerHTML = "<span> &nbsp;Show Navigation</span>"; // change text in button to reflect the action of the button
          mainNav.setAttribute("data-nav", "false"); // hide the navigation element
          isClicked = "false"; // update value of isClicked
          navContainer.style.width = "0"; // hide container so content under can be interactied with.
-         navButton.classList.remove("icon-menu"); // hide close icon
+         // navButton.classList.remove("icon-menu"); // hide close icon
       } else {
          //console.log("There is a problem with the mobile navigation");
       }
-   })
-};
+   });
+}
 
-
-
-var docBody = document.body // Get body
-
+var docBody = document.body; // Get body
 
 if (document.getElementById("show-nav").getAttribute("display") !== "none") {
    // - - - - - - - - - - - - - - - - - - - -
    // Set top offset for header element on mobile
-   // - - - - - - - - - - - - - - - - - - - - 
+   // - - - - - - - - - - - - - - - - - - - -
    var navbarElement = document.getElementById("show-nav"), // Get mobile navbar element
       navbarOffset = navbarElement.offsetHeight; // Get height of mobile navbar element
 
@@ -117,23 +140,32 @@ if (document.getElementById("show-nav").getAttribute("display") !== "none") {
       regNav.className = "sticky";
       docBody.style.marginTop = stickyOffset + "px";
    } else {
-      if (document.addEventListener) { // If happy browser
-         window.addEventListener("scroll", function () { // add scroll event
-            if (stickyNav === regNav) { // if sticky nav element is the same as first instance of navigation
-               if (window.pageYOffset >= headerOffset) { // if window is scrolled equal to or more than the height of the header element
+      if (document.addEventListener) {
+         // If happy browser
+         window.addEventListener("scroll", function () {
+            // add scroll event
+            if (stickyNav === regNav) {
+               // if sticky nav element is the same as first instance of navigation
+               if (window.pageYOffset >= headerOffset) {
+                  // if window is scrolled equal to or more than the height of the header element
                   regNav.className = "sticky"; // add class "sticky" to nav element
                } else {
                   regNav.className = ""; // leave class empty for nav element
                }
             }
-         })
-      } else if (document.attachEvent) { // If sad browser :( nav should simply not stick
-         window.attachEvent("onscroll", function () { // add scroll event
-            if (stickyNav === regNav) { // if sticky nav element is the same as first instance of navigation
-               if (window.pageYOffset >= headerOffset) { // if window is scrolled equal to or more than the height of the header element
+         });
+      } else if (document.attachEvent) {
+         // If sad browser :( nav should simply not stick
+         window.attachEvent("onscroll", function () {
+            // add scroll event
+            if (stickyNav === regNav) {
+               // if sticky nav element is the same as first instance of navigation
+               if (window.pageYOffset >= headerOffset) {
+                  // if window is scrolled equal to or more than the height of the header element
                   regNav.className = "sticky"; // add class "sticky" to nav element
                   // show logo in nav after it sticks to top
-                  if (window.innerWidth > 1400) { // only show logo in nav if above medium breakpoint
+                  if (window.innerWidth > 1400) {
+                     // only show logo in nav if above medium breakpoint
                      navLogo.style.display = "inline-block";
                   }
                } else {
@@ -141,7 +173,7 @@ if (document.getElementById("show-nav").getAttribute("display") !== "none") {
                   navLogo.style.display = "none";
                }
             }
-         })
-      };
+         });
+      }
    }
 }
